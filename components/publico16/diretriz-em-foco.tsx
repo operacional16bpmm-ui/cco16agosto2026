3f9c81b84419daf9adf61b7a152b722e64176932
Fabricio Beira Pires <verticesaopaulo@gmail.com>
2026-08-28 15:51:43 -0300

    style(cop2026): usar viatura tática no quadro dois

--- components/publico16/diretriz-em-foco.tsx
+++ components/publico16/diretriz-em-foco.tsx
@@ -12,7 +12,7 @@ const PONTOS_DIRETRIZ = [
     numero: "02",
     titulo: "ETIQUETAS DIGITAIS",
     texto: "Classificação das evidências",
-    imagem: "/media/foto-viatura.jpg",
+    imagem: "/16bpmm/carrossel/slide-12.jpg",
   },
   {
     numero: "03",
